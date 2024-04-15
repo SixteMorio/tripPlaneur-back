da95f526f3e4851e66d022f2d51185e1afe0faad
Sixte_Morio <118453359+SixteMorio@users.noreply.github.com>
2024-04-15 13:36:33 +0200

    update(route): add route addHistoy

--- routes/v1.js
+++ routes/v1.js
@@ -1,9 +1,11 @@
 import express from "express";
 
 import history from "./v1/history.js";
+import addHistory from "./v1/addHistory.js";
 
 const router = express.Router();
 
 router.use("/history", history);
+router.use("addHistory", addHistory)
 
 export default router;
