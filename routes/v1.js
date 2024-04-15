0078c69c33edbd030cd54c6e8318b9cf2babd39c
Sixte_Morio <118453359+SixteMorio@users.noreply.github.com>
2024-04-15 13:56:00 +0200

    update: add import trip  & router.use(trip)

--- routes/v1.js
+++ routes/v1.js
@@ -2,10 +2,13 @@ import express from "express";
 
 import history from "./v1/history.js";
 import addTripHistory from "./v1/addTripHistory.js";
+import trip from "./v1/trip.js";
+
 
 const router = express.Router();
 
 router.use("/history", history);
-router.use("/addTripHistory", addTripHistory)
+router.use("/addTripHistory", addTripHistory);
+router.use("/trip", trip);
 
 export default router;
