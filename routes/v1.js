e28c56dcaacbafd495f4e1594bb275e2f4aa0af5
Sixte_Morio <118453359+SixteMorio@users.noreply.github.com>
2024-04-15 15:43:21 +0200

    update: add import updateTrip

--- routes/v1.js
+++ routes/v1.js
@@ -3,6 +3,7 @@ import express from "express";
 import history from "./v1/history.js";
 import addTripHistory from "./v1/addTripHistory.js";
 import trip from "./v1/trip.js";
+import updateTrip from "./v1./updateTrip.js";
 
 
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.use("/history", history);
 router.use("/addTripHistory", addTripHistory);
 router.use("/trip", trip);
+router.use("/updateTrip", updateTrip);
 
 export default router;
