a66561abf4cfc0df6055bea124872a4464abba33
Sixte_Morio <118453359+SixteMorio@users.noreply.github.com>
2024-04-15 14:39:39 +0200

    create: add file init

---
+++ routes/v1/updateTrip.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const router = express.Router();
+
+router.patch('/trips/:id', async (req, res, next) => {
+  const { id } = req.params;
+
+  const prompt = await prisma.prompt.update({
+    where: {
+      id: Number(id),
+    },
+  });
+
+  if (!prompt) {
+    return res.status(404).json({ message: 'Prompt not found' });
+  }
+
+  res.json({ resIa: prompt.resIa });
+});
+
+export default router;
