fafb1b0af512bfa88c4309d0be684bd2587ac41e
Sixte_Morio <118453359+SixteMorio@users.noreply.github.com>
2024-04-16 13:21:14 +0200

    update: update du prompt line 8

--- routes/v1/newTrip.js
+++ routes/v1/newTrip.js
@@ -5,7 +5,9 @@ const router = express.Router();
 router.post('/', async (req, res, next) => {
   const { content, resIa } = req.body;
   const apiKey = 'iZNLEK4woJQqj5Rer3W5oI9NGuMcs4Mk';
-  const prompt = `Tu es un spécialiste d'agence de voyage, je veux que tu me fasses un itinéraire de ma destionation, que je vais te donner, en me donnant en clé json (name) le nom du lieu,`;
+  const prompt = `Tu es un spécialiste d'agence de voyage, je veux que tu me fasses le meilleur itinéraire touristique  court mais précis de mon voyage, que je vais te donner, en me donnant en clé json : (num) le numero de l'etape, (name) le nom du lieu, (km) nombre de kilometre entre chaque étape, (desc) une description rapide du lieu
+  
+  mon voyage: 3 jours en vélo dans la région PACA`;
 
   try {
 
