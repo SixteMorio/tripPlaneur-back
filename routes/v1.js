d9035ab426426fbb2bb599faeb03d21f1e25a95f
Sixte_Morio <118453359+SixteMorio@users.noreply.github.com>
2024-04-17 14:03:59 +0200

    update: delete lines import

--- routes/v1.js
+++ routes/v1.js
@@ -1,16 +1,9 @@
 import express from "express";
 
-import history from "./v1/history.js";
 import trip from "./v1/trip.js";
-import updateTrip from "./v1/updateTrip.js";
-import newTrip from "./v1/newTrip.js";
-
 
 const router = express.Router();
 
-router.use("/history", history);
 router.use("/trip", trip);
-router.use("/updateTrip", updateTrip);
-router.use("/newTrip", newTrip);
 
 export default router;
